059df8442d6335a1b0f4096b93af4686eb281df0
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-22 19:07:28 +0700

    set up caliburrn micro

---
+++ CakeShop/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeShop.ViewModels
+{
+    public class MainViewModel : Conductor<IScreen>.Collection.OneActive, INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
