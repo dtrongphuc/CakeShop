be80a5b7dd23cc17602068d10aafe29e77394fe6
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-22 19:43:37 +0700

    khởi tạo cho trang chủ

--- CakeShop/ViewModels/MainViewModel.cs
+++ CakeShop/ViewModels/MainViewModel.cs
@@ -10,9 +10,35 @@ namespace CakeShop.ViewModels
 {
     public class MainViewModel : Conductor<IScreen>.Collection.OneActive, INotifyPropertyChanged
     {
+        //private HomeViewModel _homeViewModel;
+        //public HomeViewModel HomeViewModel
+        //{
+        //    get { return _homeViewModel; }
+        //    set
+        //    {
+        //        _homeViewModel = value;
+        //        NotifyOfPropertyChange(() => HomeViewModel);
+        //    }
+        //}
+
+        /// <summary>
+        /// Khởi tạo
+        /// </summary>
         public MainViewModel()
         {
+            DisplayName = "Trang chủ";
+            ActivateItem(new HomeViewModel());
+        }
 
+        /// <summary>
+        /// Đóng view hiện tại
+        /// </summary>
+        public void CloseCurrentView()
+        {
+            while (Items.Count > 0)
+            {
+                DeactivateItem(Items[0], true);
+            }
         }
     }
 }
