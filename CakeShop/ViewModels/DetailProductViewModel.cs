cbf0c7d1822aa27a821b92c36edb0f060318cc56
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-23 22:14:37 +0700

    tạo user control chi tiết sản phẩm

---
+++ CakeShop/ViewModels/DetailProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeShop.ViewModels
+{
+    public class DetailProductViewModel : Screen
+    {
+        public DetailProductViewModel() { }
+    }
+}
