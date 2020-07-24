7d0b85a6a7cdce3372cf5e11ba9a6f2c066d91d3
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-24 15:33:41 +0700

    thử nghiệm carousel trang chi tiết

--- CakeShop/ViewModels/DetailProductViewModel.cs
+++ CakeShop/ViewModels/DetailProductViewModel.cs
@@ -11,6 +11,23 @@ namespace CakeShop.ViewModels
     public class DetailProductViewModel : Screen
     {
         //public DetailProductViewModel(string productId) { }
-        public DetailProductViewModel() { }
+        public BindableCollection<string> CarouselTest { get; set; }
+        
+        public DetailProductViewModel() 
+        {
+            CarouselTest = new BindableCollection<string>
+            {
+                "/Resource/Images/Products/detail-test.jpg",
+                "/Resource/Images/Products/1.jpg",
+                "/Resource/Images/Products/2.jpg",
+                "/Resource/Images/Products/3.jpg",
+                "/Resource/Images/Products/4.jpg",
+                "/Resource/Images/Products/5.jpg",
+                "/Resource/Images/Products/6.jpg",
+                "/Resource/Images/Products/7.jpg",
+                "/Resource/Images/Products/8.jpg",
+                "/Resource/Images/Products/9.jpg"
+            };
+        }
     }
 }
--- CakeShop/Views/DetailProductView.xaml.cs
+++ CakeShop/Views/DetailProductView.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,76 @@ namespace CakeShop.Views
         public DetailProductView()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ListView && !e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
+        }
+
+        private int _currentElement = 0;
+        private void AnimateCarousel()
+        {
+            var carousel = VisualTreeHelpers.FindChild<StackPanel>(CarouselTest, "Carousel");
+            Storyboard storyboard = (this.Resources["CarouselStoryboard"] as Storyboard);
+            DoubleAnimation animation = storyboard.Children.First() as DoubleAnimation;
+            Storyboard.SetTarget(animation, carousel);
+            animation.To = -(CarouselTest.ActualWidth + 10) * _currentElement;
+            storyboard.Begin();
+        }
+
+        private void OnPrev_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_currentElement > 0)
+            {
+                _currentElement--;
+                AnimateCarousel();
+            }
+        }
+
+        private void OnNext_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (_currentElement < 10)
+            {
+                _currentElement++;
+                AnimateCarousel();
+            }
+        }
+
+        /// <summary>
+        /// Khi chọn ảnh ở carousel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSelectedImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Border selected = sender as Border;
+            ImageSelected.Background = selected.Background as ImageBrush;
+        }
+
+        private void ScrollViewer_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //if (sender is ListView && !e.Handled)
+            //{
+            //    e.Handled = true;
+            //    var eventArg = new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, e.ChangedButton);
+            //    eventArg.RoutedEvent = UIElement.MouseLeftButtonDownEvent;
+            //    eventArg.Source = sender;
+            //    var parent = ((Control)sender).Parent as UIElement;
+            //    parent.RaiseEvent(eventArg);
+            //}
         }
     }
 }
