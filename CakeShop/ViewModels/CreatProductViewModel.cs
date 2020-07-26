80c2e5c40e6d80fe1612aa5fcae102d0bc7c9508
nguyenhieunghia2001 <nghiadx2001@gmail.com>
2020-07-26 19:52:56 +0700

    CreateView

--- CakeShop/ViewModels/CreatProductViewModel.cs
+++ CakeShop/ViewModels/CreatProductViewModel.cs
@@ -4,11 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace CakeShop.ViewModels
 {
     public class CreatProductViewModel : Screen
     {
+        public BindableCollection<string> CarouselTest { get; set; }
+        public ImageSource AddAvatar { get; set; }
 
+        public CreatProductViewModel()
+        {
+            //AddAvatar = new BitmapImage(new Uri(@"/Resource/Images/Products/detail-test.jpg", UriKind.Absolute));
+            //AddAvatar = ImageSource;
+            CarouselTest = new BindableCollection<string>
+            {
+                "/Resource/Images/Products/detail-test.jpg",
+                "/Resource/Images/Products/1.jpg",
+                "/Resource/Images/Products/2.jpg",
+                "/Resource/Images/Products/3.jpg",
+                "/Resource/Images/Products/4.jpg",
+                "/Resource/Images/Products/5.jpg",
+                "/Resource/Images/Products/6.jpg",
+                "/Resource/Images/Products/7.jpg",
+                "/Resource/Images/Products/8.jpg",
+                "/Resource/Images/Products/9.jpg"
+            };
+        }
     }
 }
