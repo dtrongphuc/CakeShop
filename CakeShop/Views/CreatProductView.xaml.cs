53e671d467a68f63cb0473d1aeabd4081b347683
vy <vsbugvery@gmail.com>
2020-07-28 22:21:16 +0700

    update

--- CakeShop/Views/CreatProductView.xaml.cs
+++ CakeShop/Views/CreatProductView.xaml.cs
@@ -173,10 +173,6 @@ namespace CakeShop.Views
             e.Handled = regex.IsMatch(e.Text);
         }
 
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
+        
     }
 }
