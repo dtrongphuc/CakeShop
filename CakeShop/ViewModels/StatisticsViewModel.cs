3102f998c15f74b8b06290cb7bc5af5bc80df859
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-29 11:31:32 +0700

    thong ke

--- CakeShop/ViewModels/StatisticsViewModel.cs
+++ CakeShop/ViewModels/StatisticsViewModel.cs
@@ -4,14 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LiveCharts;
+using LiveCharts.Wpf;
+using CakeShop.Models;
 
 namespace CakeShop.ViewModels
 {
     public class StatisticsViewModel : Screen
     {
+        public SeriesCollection CartesianChartData { get; set; } = new SeriesCollection();
+        public string[] MonthLabels { get; set; }
+        private GetListObject _getList { get; } = new GetListObject();
+
         public StatisticsViewModel ()
         {
+            
 
+            MonthLabels = new[] { "Tháng 1", "Tháng 2", "Tháng 3" };
+        }
+
+        public void GetDataForCartesianChart()
+        {
+            //Order OrderInMonth = 
+            //_getList.Get_MonthlyOrder();
+            CartesianChartData.Add(
+                new ColumnSeries
+                {
+                    Title = string.Empty,
+                    Values = new ChartValues<int> { 10, 20, 30 },
+                }
+            );
         }
     }
 }
