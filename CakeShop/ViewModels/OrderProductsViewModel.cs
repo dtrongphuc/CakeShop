193cb1ebc3ee2f295293bba189f1cc75e6e55c6c
dtrongphuc <dtrongphucdv@gmail.com>
2020-08-01 11:29:12 +0700

    chi tiết đơn hàng chưa binding được

--- CakeShop/ViewModels/OrderProductsViewModel.cs
+++ CakeShop/ViewModels/OrderProductsViewModel.cs
@@ -11,11 +11,14 @@ namespace CakeShop.ViewModels
     public class OrderProductsViewModel : Screen
     {
         public GetListObject GetData = new GetListObject();
+        public DetailOrder GetDetail = new DetailOrder();
         public BindableCollection<Order> OrdersDataGrid { get; set; }
-       
+        public BindableCollection<Product> DetailDataGrid { get; set; }
         public OrderProductsViewModel() 
         {
             OrdersDataGrid = GetData.Get_AllOrder();
+            GetDetail.Find("1");
+            DetailDataGrid = GetDetail.ListProduct;
         }
     }
 }
