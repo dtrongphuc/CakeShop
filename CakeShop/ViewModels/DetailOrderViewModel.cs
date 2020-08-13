d6c8dbdbbe93dae9d015fe4bea9aeaf2ce5e2c75
nguyenhieunghia2001 <nghiadx2001@gmail.com>
2020-08-13 11:45:44 +0700

    tạo trang chi tiết đơn hàng

---
+++ CakeShop/ViewModels/DetailOrderViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CakeShop.Models;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CakeShop.Views;
+using System.IO;
+using System.IO.Packaging;
+using CakeShop.ViewModels;
+
+namespace CakeShop.Models
+{
+    public class DetailOrderViewModel : Screen
+    {
+
+        public void ShowOrder()
+        {
+            var parentConductor = (Conductor<IScreen>.Collection.OneActive)(this.Parent);
+            Conductor<IScreen>.Collection.OneActive MainConductor = (Conductor<IScreen>.Collection.OneActive)parentConductor;
+            MainConductor.DeactivateItem(MainConductor.Items[0], true);
+            parentConductor.ActivateItem(new OrderProductsViewModel());
+        }
+    }
+
+    
+}
