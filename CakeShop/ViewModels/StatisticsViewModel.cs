dff5bc10d52f8b9aeccb129292026aa2fbcbb9b1
dtrongphuc <dtrongphucdv@gmail.com>
2020-08-13 17:23:22 +0700

    sửa giao diện thêm đơn hàng

--- CakeShop/ViewModels/StatisticsViewModel.cs
+++ CakeShop/ViewModels/StatisticsViewModel.cs
@@ -13,18 +13,38 @@ namespace CakeShop.ViewModels
     public class StatisticsViewModel : Screen
     {
         public SeriesCollection CartesianChartData { get; set; } = new SeriesCollection();
+        public SeriesCollection PieChart { get; set; } = new SeriesCollection();
+
         public string[] MonthLabels { get; set; }
         private GetListObject _getList { get; } = new GetListObject();
 
-        public StatisticsViewModel ()
+        public StatisticsViewModel()
         {
+            RevenueByCategory();
             GetDataForCartesianChart();
 
             MonthLabels = new[] {
                 "Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6",
                 "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"
             };
-            
+        }
+
+        public void GetDataForPieChart()
+        {
+            BindableCollection<Category> Categories = _getList.Get_AllCategory();
+            BindableCollection<Order> AllOrder = _getList.Get_AllOrder();
+            foreach (Category CategoryItem in Categories)
+            {
+
+            }
+        }
+
+        public void RevenueByCategory()
+        {
+            BindableCollection<Category> Categories = _getList.Get_AllCategory();
+            BindableCollection<Order> AllOrder = _getList.Get_AllOrder();
+            DetailOrder Detail = new DetailOrder();
+            Detail.Find("1");
         }
 
         public void GetDataForCartesianChart()
